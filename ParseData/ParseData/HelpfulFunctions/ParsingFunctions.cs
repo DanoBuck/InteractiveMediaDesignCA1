97f72cb84d0954a6320e8a6424b85405381e5ad2
DanoBuck <X00109141@gmail.com>
2016-10-29 15:30:28 +0100

    Work done on CA so far

---
+++ ParseData/ParseData/HelpfulFunctions/ParsingFunctions.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+
+namespace ParseData.HelpfulFunctions
+{
+    public class ParsingFunctions
+    {
+        public static string getMonth(string month)
+        {
+            string result = "";
+            if (month == "01")
+            {
+                result = "January";
+            }
+            else if (month == "02")
+            {
+                result = "February";
+            }
+            else if (month == "03")
+            {
+                result = "March";
+            }
+            else if (month == "04")
+            {
+                result = "April";
+            }
+            else if (month == "05")
+            {
+                result = "May";
+            }
+            else if (month == "06")
+            {
+                result = "June";
+            }
+            else if (month == "07")
+            {
+                result = "July";
+            }
+            else if (month == "08")
+            {
+                result = "August";
+            }
+            else if (month == "09")
+            {
+                result = "September";
+            }
+            else if (month == "10")
+            {
+                result = "October";
+            }
+            else if (month == "11")
+            {
+                result = "November";
+            }
+            else if (month == "12")
+            {
+                result = "December";
+            }
+            return result;
+        }
+
+        public static string ParseThisDataByMonth(String dataToParse)
+        {
+            char[] month;
+            string months = "";
+            string resultToSplitAgain = "";
+            string[] dataParsing = dataToParse.Split('M');
+            foreach (string str in dataParsing)
+            {
+                month = str.ToCharArray();
+                months = month[0].ToString() + month[1].ToString();
+                resultToSplitAgain += getMonth(months) + "\n";
+            }
+
+            return resultToSplitAgain;
+        }
+
+        public static string ParseThisDataByYear(String dataToParse)
+        {
+            string result = "";
+            char[] yearArray;
+            string[] dataParsing = dataToParse.Split('M');
+            foreach (string str in dataParsing)
+            {
+                yearArray = str.ToCharArray();
+                if (yearArray.Length > 6)
+                {
+                    result += yearArray[3].ToString() + yearArray[4].ToString() + yearArray[5].ToString() + yearArray[6].ToString() + "\n";
+                }
+                else if (yearArray.Length == 4)
+                {
+                    result += str + "\n";
+                }
+            }
+            return result;
+        }
+
+        public static void PrintOutMonths29Times()
+        {
+            string[] months = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+
+            StreamWriter sw = new StreamWriter("C:\\Users\\Daniel\\Desktop\\College Work\\InteractiveMediaDesign\\CA1\\PenaltyPointsData\\Months.txt");
+
+
+            for (int i = 0; i < months.Length; i++)
+            {
+                for (int j = 0; j < 29; j++)
+                {
+                    sw.WriteLine(months[i]);
+                }
+            }
+            sw.Close();
+        }
+    }
+}
---
+++ ParseData/ParseData/PenaltyPoints/PenaltyPointParsing.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace ParseData.PenaltyPoints
+{
+    public class PenaltyPointParsing
+    {
+        // There has to be a better way of doing this but it is working now
+        public static void ParseAllDataIntoDifferentFormatsForPenaltyPointDataForFirstStage()
+        {
+            List<string> listOfSections = new List<string>();
+            var values = File.ReadAllLines("C:\\Users\\Daniel\\Desktop\\College Work\\InteractiveMediaDesign\\CA1\\PenaltyPointsData\\PenaltyPointsParsingCSV.csv");
+            StreamWriter sw = new StreamWriter("C:\\Users\\Daniel\\Desktop\\College Work\\InteractiveMediaDesign\\CA1\\PenaltyPointsData\\MonthByMonthPenaltyPointParsing.txt");
+            string holdThisHere = "";
+
+            int counter = 0;
+            // make into char array
+            char[] charArray;
+
+            int cycles = 29;
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                charArray = values[i].ToCharArray();
+                holdThisHere = "";
+                for (int j = 0; j < 1; j++)
+                {
+                    holdThisHere += values[i];
+                    listOfSections.Add(holdThisHere);
+                    counter++;
+                }
+                if (counter == cycles)
+                {
+                    counter = 0;
+                }
+            }
+            int lineBreaks = 67;
+            for (int i = 0; i < listOfSections.Count; i++)
+            {
+                if (i % lineBreaks == 0)
+                {
+                    for (int j = 0; j < 1; j++)
+                    {
+                        sw.WriteLine();
+                    }
+                    sw.Write(listOfSections[i] + "\t");
+                }
+                else if (i % lineBreaks != 0)
+                {
+                    sw.Write(listOfSections[i] + "\t");
+                }
+            }
+            sw.Close();
+        }
+
+        public static void ParseBlanksAndDashesForPenaltyPointDataForSecondStage()
+        {
+            List<string> listOfSections = new List<string>();
+            var values = File.ReadAllLines("C:\\Users\\Daniel\\Desktop\\College Work\\InteractiveMediaDesign\\CA1\\PenaltyPointsData\\PenaltyPointsParsingCSV.csv");
+            StreamWriter sw = new StreamWriter("C:\\Users\\Daniel\\Desktop\\College Work\\InteractiveMediaDesign\\CA1\\PenaltyPointsData\\MonthByMonthPenaltyPointParsingBlanksAndDashes.csv");
+
+            char[] charArray;
+            string holdThisHere = "";
+            for (int i = 0; i < values.Length; i++)
+            {
+                charArray = values[i].ToCharArray();
+                holdThisHere = "";
+                for (int j = 0; j < charArray.Length; j++)
+                {
+                    if (charArray[j] == ' ' || charArray[j] == '-')
+                    {
+                        holdThisHere += "0";
+                    }
+                    else {
+                        holdThisHere += charArray[j];
+                    }
+                    listOfSections.Add(holdThisHere);
+                }
+            }
+            for (int i = 0; i < listOfSections.Count; i++)
+            {
+                sw.Write(listOfSections[i]);
+            }
+            sw.Close();
+        }
+    }
+}
